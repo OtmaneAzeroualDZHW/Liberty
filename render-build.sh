170dc6f80958c3de369f0e3dadc90517fd6f01f8
OtmaneAzeroualDZHW <56314235+OtmaneAzeroualDZHW@users.noreply.github.com>
2026-01-13 16:30:16 +0100

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-# Update Paketlisten
-sudo apt-get update
-
-# Installiere Poppler (für PDF-Konvertierung)
-sudo apt-get install -y poppler-utils
-
-# Installiere Tesseract OCR
-sudo apt-get install -y tesseract-ocr
-
-# Optional: Deutsche Sprache für Tesseract
-sudo apt-get install -y tesseract-ocr-deu
-
+#!/usr/bin/env bash
+pip install --upgrade pip
+pip install -r requirements.txt
