aa685b004f92275554a0d29ea0323f90c9e2bbdd
OtmaneAzeroualDZHW <56314235+OtmaneAzeroualDZHW@users.noreply.github.com>
2026-01-13 16:50:12 +0100

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
-pip install --upgrade pip
+
+# Update System
+apt-get update
+
+# Poppler installieren (für pdf2image)
+apt-get install -y poppler-utils
+
+# Tesseract OCR installieren
+apt-get install -y tesseract-ocr tesseract-ocr-deu tesseract-ocr-eng
+
+# Python-Abhängigkeiten installieren
 pip install -r requirements.txt
